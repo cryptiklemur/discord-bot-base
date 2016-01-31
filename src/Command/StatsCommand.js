f34af3bbdadb5201d36564253a092b2f283ced6a
Aaron Scherer <aequasi@gmail.com>
2016-01-31 11:51:32 -0800

    Updating stats to show current server

--- src/Command/StatsCommand.js
+++ src/Command/StatsCommand.js
@@ -12,9 +12,24 @@ class StatsCommand extends AbstractCommand {
                 users    = this.client.users.length,
                 online   = this.client.users.filter(u => u.status != "offline").length;
 
-            this.reply(
-                `Currently joined to: ${servers} servers with ${online}/${users} members and ${channels} text channels.`
+            this.sendMessage(
+                this.message.channel,
+                `Currently joined to: ${servers} servers with ${online}/${users} members and ${channels} channels.`
             );
+
+            if (!this.message.isPm()) {
+                channels = this.message.server.channels.length,
+                    users    = this.message.server.members.length,
+                    online   = this.message.server.members.filter(u => u.status != "offline").length;
+
+                this.sendMessage(
+                    this.message.channel,
+                    `The current server has ${online}/${users} members and ${channels} channels.`,
+                    500
+                );
+
+            }
+
         });
     }
 }
