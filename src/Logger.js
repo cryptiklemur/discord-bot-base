706de3b9bf34fa3f560b274405c2b04a0929c9b3
Aaron Scherer <aequasi@gmail.com>
2016-02-08 19:30:00 -0800

    Removing rotate in favor of logrotate

--- src/Logger.js
+++ src/Logger.js
@@ -1,17 +1,13 @@
 const winston = require('winston');
-const Rotate  = require('winston-logrotate').Rotate;
 
 module.exports = function(debug, log_dir, name) {
     var logger = new (winston.Logger)({
         transports: [
-            new (Rotate)({
+            new (winston.transports.File)({
                 file:      `${log_dir}/${name}.log`,
                 colorize:  false,
                 timestamp: true,
-                json:      true,
-                max:       '100m',
-                keep:      7,
-                compress:  true
+                json:      true
             }),
             new (winston.transports.Console)({
                 prettyPrint:      true,
